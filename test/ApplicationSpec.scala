5c739285ea470e41fd55aa6327f2e005f3944fd2
Ethan Eldridge <ejayeldridge@gmail.com>
2015-12-03 10:17:48 -0500

    Add rfc notes

--- test/ApplicationSpec.scala
+++ test/ApplicationSpec.scala
@@ -138,6 +138,24 @@ class ApplicationSpec extends Specification {
         FakeController.rfc2616ParseHeader(header) must equalTo(expectedLang) 
     }
 
+    /** 
+     * The BNF says:      Accept-Language = 1#( language-range [ weight ] ) 
+     * Which means that we have a list of ranges of at least 1 and as many as 
+     * we want. The full form of # lists are "<n>#<m>element" indicating at 
+     * least <n> and at most <m> elements, each separated by one or more commas
+     * (",") and OPTIONAL linear white space (LWS). Because the spec also says:
+     * "A recipient MAY replace any linear white space with a single SP before 
+     * interpreting the field value or forwarding the message downstream." We 
+     * are at liberty to ignore runs of whitespace and treat them as one. 
+     * tl;dr you can put as much whitespace as you want between language tags
+     * @see http://www.w3.org/Protocols/rfc2616/rfc2616-sec2.html
+     */
+    "parse a RFC 2616 compliant header with multiple languages and whitespace" in {
+        val expectedLang = Lang("en", "gb")
+        val header = "da;q=0.1,    en-gb;q=0.8,     en;q=0.7"
+        FakeController.rfc2616ParseHeader(header) must equalTo(expectedLang) 
+    }
+
     "parse a RFC 2616 compliant header with no q values" in {
         val expectedLang = Lang("de", "")
         val header = "de,da, en-gb, en"
@@ -150,6 +168,16 @@ class ApplicationSpec extends Specification {
         FakeController.rfc2616ParseHeader(header) must equalTo(expectedLang) 
     }
 
+    /**
+     * If the header field is present in a request and none of the available 
+     * representations for the response have a matching language tag, the origin 
+     * server can either disregard the header field by treating the response as 
+     * if it is not subject to content negotiation or honor the header field by
+     * sending a 406 (Not Acceptable) response.  However, the latter is not
+     * encouraged, as doing so can prevent users from accessing content that
+     * they might be able to use (with translation software, for example).
+     * @see http://tools.ietf.org/html/rfc7231#section-5.3.5
+     */
     "return a default if an empty header is sent" in {
         val expectedLang = play.api.i18n.Lang.defaultLang
         val header = ""
